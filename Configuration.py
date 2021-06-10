1b253afab93ca08822908ca29383682439fbde9a
Jorge Beyoglonian <jbeyoglo@gmail.com>
2021-06-09 20:15:27 -0500

    default configuration

---
+++ Configuration.py
@@ -0,0 +1,15 @@
+
+camera = {
+    "resX": 1280,
+    "resY": 720,
+    "secondsToFocus": 2,
+    "fontSize": "my secret password"
+}
+
+notification = {
+    "key": "my secret pushsafer.com key"
+}
+
+proofer = {
+    "initialTargetTemperature": 28
+}
